8801e6495460fbd06dd8d9047cfdcaabcb9e7ee9
Sanket Sarang <sanket@blobcity.com>
2018-09-09 12:29:01 +0530

    Removed unwanted files

--- inmemory_cube.sh
+++
@@ -1,3 +0,0 @@
-create-table cubetest.ul1 --t in-memory --flexible true
-populate-table cubetest.ul1 /Users/ashish/Downloads/ULData/2007/CivilCas.csv true , ''
-create-data-cube cubetest.ul1 STATE AGE RACE ETHNICITY
