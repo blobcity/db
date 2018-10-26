b7577feda02a1b40fe1f3382727f744e889d1d51
Sanket Sarang <sanket@blobcity.com>
2018-10-26 19:29:58 +0530

    Updated blobcity.sh file

--- scripts/blobcity.sh
+++ scripts/blobcity.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 #Copyright (C) 2018  BlobCity, Inc
 
 #This program is free software: you can redistribute it and/or modify
@@ -13,7 +15,7 @@
 #You should have received a copy of the GNU Affero General Public License
 #along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-#!/usr/bin/env bash
+
 
 # Set the BlobCity specific environment and add all required libraries to the classpath
 
