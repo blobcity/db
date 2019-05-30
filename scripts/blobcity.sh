aed77a4b6431de5877aed5f289238e46bf71f6f5
Sanket Sarang <sanket@blobcity.com>
2019-05-31 00:38:59 +0530

    Version change to 1.7.8-alpha

--- scripts/blobcity.sh
+++ scripts/blobcity.sh
@@ -22,7 +22,7 @@
 # The Java implementation to use. This is required.
 #export JAVA_HOME=
 
-VERSION="1.7.7-alpha"
+VERSION="1.7.8-alpha"
 JAVA=""
 if [ "$JAVA_HOME" != "" ]; then
   JAVA=$JAVA_HOME/bin/java
